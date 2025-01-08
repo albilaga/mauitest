855840ad79115e4cf233101f91cb06ed4632481a
Albilaga Linggra Pradana <git@albilaga.id>
2025-01-08 16:15:27 +0700

    remove unneeded files

--- mauitest/mauitest/IFileDownloadManager.cs
+++
@@ -1,9 +0,0 @@
-namespace mauitest
-{
-    public interface IFileDownloadManager
-    {
-        Task<byte[]> DownloadFileFromUrlAsync(string url, CancellationToken cancellationToken = default);
-
-        void PreloadFileFromUrl(string url);
-    }
-}
